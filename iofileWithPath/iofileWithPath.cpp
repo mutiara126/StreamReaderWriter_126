1ce0bf61bb1222aae6863403b3e38f49e5884059
mutiara126 <salsamutiara1812@gmail.com>
2023-06-19 11:55:05 +0800

    untuk menampilkan file

---
+++ iofileWithPath/iofileWithPath.cpp
@@ -0,0 +1,14 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string baris;
+	string nama file;
+
+	cout << "Masukan Nama File : ";
+	cin >> NamaFile;
+
+}
+

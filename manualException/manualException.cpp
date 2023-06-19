1d2e9b23a0aa6549185cc2171d44611a23791bf5
mutiara126 <salsamutiara1812@gmail.com>
2023-06-19 12:16:59 +0800

    untuk membuat sebuah integer

--- manualException/manualException.cpp
+++ manualException/manualException.cpp
@@ -5,5 +5,7 @@ int main()
 {
 	try {
 		cout << "Selamat Belajar di Prodi TI UMY" << endl;
+		//throw 0.5; //melemparkan sebuah integar maka
+		cout << "pernyataan tidak akan dieksekusi" << endl;
 	}
 }

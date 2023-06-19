51332890181241fab8dca4fe8456b53f764ae1a9
mutiara126 <salsamutiara1812@gmail.com>
2023-06-19 11:32:53 +0800

    untuk memasukan baris kedalam file

--- iofile/iofile.cpp
+++ iofile/iofile.cpp
@@ -19,6 +19,10 @@ int main() {
 		getline(cin, baris);
 		//loop akan berhenti jika anda memasukan karakter q
 		if (baris == "q") break;
+		//menulis dan memasukan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
+
+
 		
 	}
 

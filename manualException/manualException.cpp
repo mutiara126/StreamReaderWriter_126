c9642005f82af898bb57705d851ed0a0dea580a3
mutiara126 <salsamutiara1812@gmail.com>
2023-06-19 12:35:29 +0800

    exceptionStandar

--- manualException/manualException.cpp
+++ manualException/manualException.cpp
@@ -8,4 +8,13 @@ int main()
 		//throw 0.5; //melemparkan sebuah integar maka
 		cout << "pernyataan tidak akan dieksekusi" << endl;
 	}
+	catch (int a) {
+		//blok ini akan dieksekusi
+		cout << "pengecualian akan dieksekusi" << endl;
+    }
+	catch (...) {
+		/*jika selain integer maka block ini akan dieksekusi*/
+		cout << "Default pengecualian dieksekusi" << endl;
+	}
+	return 0;
 }
